1adb79c66962819a164cd4e4a20808daff8f7dd9
cgWoofus <charlesbryanguerrero@yahoo.com>
2018-03-13 18:57:18 +0800

    Initial commit

---
+++ BlockCollection.cs
@@ -0,0 +1,147 @@
+﻿
+using UnityEngine;
+using UnityEditor;
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using smartReplace;
+
+namespace smartReplace
+{
+
+    public enum BGTYPE { BG0, BG1, BG2 };
+    public enum OblockSize { Small, Medium, Large }
+    public enum LockedSides
+    {
+        UL = (1 << 7),
+        UR = (1 << 6),
+        DL = (1 << 5),
+        DR = (1 << 4),
+        U = (1 << 3),
+        D = (1 << 2),
+        L = (1 << 1),
+        R = (1 << 0)
+    }
+
+    [System.Flags]
+    public enum OLayer
+    {
+        Layer0 = (1 << 0),
+        Layer1 = (1 << 1),
+        Layer2 = (1 << 2),
+        Spike = (1 << 3),
+    }
+    public enum OblockType { A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z, AA, BB, CC, DD, EE, FF, GG, HH, II, JJ, KK, LL, MM, NN, OO, PP, QQ, RR, SS, TT, UU };
+
+
+}
+
+
+
+[System.Serializable]
+public class BlockCollection : ScriptableObject
+{
+    public GameObject _initBlock;
+    public GameObject[] _blockTypes;
+    public bool _adaptive = false;
+    public BGTYPE _bgType;
+    //public OblockBehaviour _oBehavior;
+    
+    
+    public OblockSize _collectionSize;
+    [SerializeField][EnumFlag]
+    public OLayer _oLayerFlag;
+
+    [SerializeField][EnumFlag]
+    public LockedSides _sideLockFlag;
+
+    public void SetBlocks(GameObject[] _blockList,string prefabPath)
+    {
+        _blockTypes = new GameObject[47];
+        var _path = prefabPath;// PlayerConstants.GlobalConstants()._prefabObjectPath;
+        var _blockP = _blockList.Length - 1;
+        for (int x = 0; x < _blockList.Length; x++)
+        {
+
+            var _prefab = AssetDatabase.LoadAssetAtPath(string.Format(_path, _blockList[x].name), typeof(GameObject));
+
+            int num;
+
+            var suffnum = int.TryParse(_prefab.name.Split('_')[3].Substring(3), out num);
+
+          //  Debug.Log(num);
+
+            _blockTypes[num] = (GameObject)_prefab;
+
+            if (x == _blockP)
+               SetInitBlock((GameObject)_prefab);
+        };
+
+
+    }
+
+
+    public void SetInitBlock(GameObject block)
+    {
+        _initBlock = block;
+    }
+
+    public GameObject GetInitBlock
+    {
+        get {
+            if (_initBlock != null)
+                return _initBlock;
+            else
+              return  GetFromMesh();
+            }
+
+    }
+
+    GameObject GetFromMesh()
+    {
+        throw new NotImplementedException();
+    }
+    public void SetBlockTypes(params GameObject[] blocks)
+    {
+
+        _blockTypes = new GameObject[blocks.Length];
+        for(int x=0;x<blocks.Length;x++)
+        {
+            _blockTypes[x] = blocks[x];
+        }
+    }
+
+
+
+    public float GetRayDistanceCast
+    {
+        get
+        {
+            switch(_collectionSize)
+            {
+                case OblockSize.Small:
+                    return 2f;
+                case OblockSize.Medium:
+                    return 2.3f;
+                case OblockSize.Large:
+                    return 4.1f;
+                default:
+                    return 0f;
+            }
+
+        }
+
+    }
+    
+    public bool CompareBlock<T>(GameObject obj, T comparer)where T : IEqualityComparer<GameObject>
+    {
+        //var comparer = new BlockComparer();
+        bool val =  _blockTypes.Contains(obj, comparer);
+        return val;
+    }
+
+
+
+
+}
+
---
+++ BlockHitList.cs
@@ -0,0 +1,142 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using smartReplace;
+
+[System.Serializable]
+public class BlockHitList : ScriptableObject 
+{
+    public HitObject[] listOfHit;
+
+
+    public BlockHitList(int listSize)
+    {
+        listOfHit = new HitObject[listSize];
+    }
+
+    public void setPath(int index, Vector2[] points)
+    {
+        try
+        {
+            listOfHit[index] = new HitObject();
+            listOfHit[index].points = new Vector2[points.Length];
+
+
+            points.CopyTo(listOfHit[index].points, 0);
+        }
+        catch
+        {
+            if (points == null)
+                Debug.Log("no path found");
+            else
+                Debug.Log("error copying path");
+        }
+    }
+
+    public void setPath2(int index,string objName, List<Vector2[]> paths)
+    {
+        listOfHit[index] = new HitObject();
+        listOfHit[index].objName = objName;
+        if (paths == null)
+            throw new System.Exception("paths is null");
+
+        listOfHit[index].path = new List<Vector2[]>();
+        for (int x = 0; x< paths.Count;x++)
+        {
+            if(paths[x].Length<1)
+            {
+                Debug.Log("path with zero length copied is this intended?");
+            }
+            listOfHit[index].path.Add(paths[x]);
+        }
+
+    }
+
+    public void setPath3(int index, string objName, List<Vector2[]> paths)
+    {
+        listOfHit[index] = new HitObject();
+        listOfHit[index].objName = objName;
+        if (paths == null)
+            throw new System.Exception("paths is null");
+
+        listOfHit[index].go = new List<ListOfPoints>();
+        for (int x = 0; x < paths.Count; x++)
+        {
+            if (paths[x].Length < 1)
+            {
+                Debug.Log("path with zero length copied is this intended?");
+                continue;
+            }
+            listOfHit[index].go.Add(new ListOfPoints());
+            listOfHit[index].go[x]._pointList = new Vector2[paths[x].Length];
+            paths[x].CopyTo(listOfHit[index].go[x]._pointList, 0); 
+        }
+
+    }
+
+    public void clean()
+    {
+        var cleanList = new List<HitObject>();
+
+        for (int x = 0; x < this.listOfHit.Length; x++)
+        {
+            if (this.listOfHit[x] != null)
+            {
+                cleanList.Add(this.listOfHit[x]);
+            }
+        }
+        listOfHit = new HitObject[cleanList.Count];
+        for (int z = 0; z < listOfHit.Length; z++)
+            listOfHit[z] = cleanList[z];
+
+    }
+
+    public static BlockHitList  createList(List<Vector2[]> listOfObjects,string filename)
+    {
+        var bhlist = new BlockHitList(listOfObjects.Count);
+        try
+        {
+            for (int x = 0; x < listOfObjects.Count; x++)
+            {
+                bhlist.setPath(x, listOfObjects[x]);
+                
+            }
+        }
+        catch
+        {
+            Debug.Log("Problem on BlockHitList Class");
+        }
+        return bhlist;
+    }
+
+
+    public static BlockHitList createList2(List<List<Vector2[]>> listOfObjects,Transform[] _allTransformList, string fileName)
+    {
+       var bhList = new BlockHitList(listOfObjects.Count);
+        for (int x = 0; x < _allTransformList.Length; x++)
+        {
+
+            if (_allTransformList[x] == null)
+                continue;
+
+            if (listOfObjects[x] == null)
+                continue;
+
+            var anObject = new List<Vector2[]>();     
+                        
+            for (int y = 0; y < listOfObjects[x].Count; y++)
+                {
+                    if (listOfObjects[x][y] != null && listOfObjects[x][y].Length>0)
+                        anObject.Add(listOfObjects[x][y]);
+            }
+
+            if(anObject.Count>0)
+                {
+                  bhList.setPath3(x, _allTransformList[x].name, anObject);
+                }
+        }
+
+        bhList.clean();
+        return bhList;
+    }
+}
